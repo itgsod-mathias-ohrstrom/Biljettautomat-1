055a12d12e3a7570346bc092384f5823af5368b9
itgsod-mathias-ohrstrom <mathias.ohrstrom@elev.it-gymnasiet.se>
2014-09-30 13:50:29 +0200

    ticket
    small changes

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,17 +1,17 @@
 def ask_age
   puts "Enter your age"
-  age = gets.to_i
-  return age
+  return gets.to_i
 end
 
-def ticket_price(ask_age)
-  price = 0
-  if ask_age < 18
+def ticket_price(age)
+  if age < 18
     price = 10
-  elsif ask_age < 65
+  elsif age < 65
     price = 20
   else
     price = 15
   end
   return price
 end
+
+puts "The price is #{ticket_price(ask_age)}"
